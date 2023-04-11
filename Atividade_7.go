fd5697db308c7ec89eb282e30026b9842edbda22
ItzTutu <104566356+tutupro@users.noreply.github.com>
2023-04-11 16:38:59 -0300

    Update Atividade_7.go

--- Atividade_7.go
+++ Atividade_7.go
@@ -1,31 +1,30 @@
+Codigo De funções
+
+Atividade 1º:
+
 package main
 
 import "fmt"
 
-// Crie uma função que receba um slice de inteiros
-//e retorne a média aritmética dos valores.
-
 func main() {
-
 	var z int
-
-	x := []int{}
-
+	slice := []int{}
 	fmt.Print("Digite números para serem inseridos no slice: ")
 	fmt.Scan(&z)
 
-	x = append(x, z)
-	resultado := media(x)
-	fmt.Println(resultado)
-
+	x := append(slice, z)
+	println(x)
+	media := mediaAritmetica(slice)
+	fmt.Println(media)
 }
-func media(x []int) int {
 
-	resultado := 0
-
-	for _, x := range x {
-		resultado += x
+func mediaAritmetica(slice []int) float64 {
+	soma := 0
+	for _, valor := range slice {
+		soma += valor
 	}
-	return len(x) / resultado
-
+	return float64(soma) / float64(len(slice))
 }
+
+Atividade 2º:
+
