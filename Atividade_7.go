09d6dd2c12d5116b2848e608ce32b64b2682d149
ItzTutu <104566356+tutupro@users.noreply.github.com>
2023-04-11 09:08:17 -0300

    Add files via upload

---
+++ Atividade_7.go
@@ -0,0 +1,13 @@
+package main
+
+// Crie uma função que receba dois parâmetros (a e b) e retorne a divisão
+// entre eles. Caso o segundo parâmetro seja zero
+// retorne um erro.
+
+func main() {
+
+}
+func teste(a int64, b int64) int64 {
+	// codigo
+	return a / b
+}
